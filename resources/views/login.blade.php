426b1bac2ae258ffafdb88300923b882de47c6e4
syarifulanam <em.syariful.anam@gmail.com>
2025-05-13 12:42:44 +0700

    login and logout

---
+++ resources/views/login.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Login Page</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <style>
+   body {
+        background-color: #f4f4f4;
+        height: 100%;
+        margin: 0;
+    }
+    .login-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+    .login-box {
+        width: 100%;
+        max-width: 400px;
+        padding: 30px;
+        background-color: white;
+        border-radius: 8px;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+    }
+    .login-box h3 {
+        text-align: center;
+        margin-bottom: 30px;
+    }
+    .form-control {
+        border-radius: 6px; /* instead of 25% */
+    }
+    .btn-primary {
+        border-radius: 25px;
+    }
+    .forgot-password {
+        text-align: center;
+        margin-top: 15px;
+    }
+  </style>
+</head>
+<body>
+    
+  <div class="container d-flex justify-content-center align-items-center login-container">
+    <div class="p-4 shadow card" style="width: 100%; max-width: 400px;">
+      <h3 class="mb-4 text-center">Login</h3>
+      <form action="login" method="POST">
+        @csrf
+        <div class="mb-3">
+          <label for="email" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="email"  name="email" required placeholder="Enter your email">
+        </div>
+        
+        <div class="mb-3">
+          <label for="password" class="form-label">Password</label>
+          <input type="password" class="form-control" id="password" name="password" required placeholder="Enter your password">
+        </div>
+        <div class="gap-2 d-grid">
+        <button type="submit" class="btn btn-primary w-100">Login</button>
+        </div>
+      </form>
+
+      <div class="mt-3 text-center">
+        <a href="#">Forgot Password?</a>
+      </div>
+    </div>
+    
+    @if($errors->any())
+        <div class="mt-3 alert alert-danger col-md-6" style="max-width: 400px">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,49 @@
 <?php
 
+use App\Models\Image;
 use App\Models\Phone;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ImageController;
-use App\Models\Image;
+use App\Http\Controllers\CommentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/blog', [BlogController::class, 'index'])->name('blog');
-Route::get('/blog/add', [BlogController::class, 'add']);
-Route::post('/blog/create', [BlogController::class, 'create']);
-Route::get('/blog/{id}/detail', [BlogController::class, 'show'])->name('blog-detail');
-Route::get('/blog/{id}/edit', [BlogController::class, 'edit']);
-Route::patch('/blog/{id}/update', [BlogController::class, 'update']);
-Route::get('/blog/{id}/delete', [BlogController::class, 'delete']);
-Route::get('/blog/{id}/restore', [BlogController::class, 'restore']);
+Route::middleware('auth')->group(function () {
+    Route::get('/blog', [BlogController::class, 'index'])->name('blog');
+    Route::get('/blog/add', [BlogController::class, 'add']);
+    Route::post('/blog/create', [BlogController::class, 'create']);
+    Route::get('/blog/{id}/detail', [BlogController::class, 'show'])->name('blog-detail');
+    Route::get('/blog/{id}/edit', [BlogController::class, 'edit']);
+    Route::patch('/blog/{id}/update', [BlogController::class, 'update']);
+    Route::get('/blog/{id}/delete', [BlogController::class, 'delete']);
+    Route::get('/blog/{id}/restore', [BlogController::class, 'restore']);
 
-Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users', [UserController::class, 'index']);
 
-Route::get('/phones', function () {
-    $phones = Phone::with('user')->get();
-    return $phones;
+    Route::get('/phones', function () {
+        $phones = Phone::with('user')->get();
+        return $phones;
+    });
+
+    Route::get('/comment', [CommentController::class, 'index']);
+    Route::post('/comment/{blog_id}', [CommentController::class, 'store']);
+
+    Route::get('/images', [ImageController::class, 'index']);
+
+    Route::get('/logout', [AuthController::class, 'logout']);
 });
 
-Route::get('/comment', [CommentController::class, 'index']);
-Route::post('/comment/{blog_id}', [CommentController::class, 'store']);
-Route::get('/images', [ImageController::class, 'index']);
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticating']);
+});
+
 // Route::get('/blog', function () {
 //     //ambil dari database
 //     $profile = 'aku programmer noob';
